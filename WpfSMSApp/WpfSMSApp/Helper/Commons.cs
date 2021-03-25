9b441213e7f81c69295fa64ea0afbf02c97d097f
WhiteHair-H <https://github.com/WhiteHair-H/BasicSetting.git>
2021-03-25 17:25:22 +0900

    3월 25일 커밋

---
+++ WpfSMSApp/WpfSMSApp/Helper/Commons.cs
@@ -0,0 +1,25 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfSMSApp.Model;
+
+namespace WpfSMSApp
+{
+
+    public class Commons
+    {
+    // NLOG 정적 인스턴스 생성
+    public static readonly Logger LOGGER = LogManager.GetCurrentClassLogger();
+
+        // 로그인한 유저 정보
+        public static User LOGINED_USER;
+
+
+
+
+
+    }
+}
