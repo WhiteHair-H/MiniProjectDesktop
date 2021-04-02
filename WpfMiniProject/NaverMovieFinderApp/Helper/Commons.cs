8545ddfaeda3faa7f9887003c08ebf1324015087
WhiteHair-H <https://github.com/WhiteHair-H/BasicSetting.git>
2021-04-02 16:33:42 +0900

    네이버 영화검색 커밋

--- WpfMiniProject/NaverMovieFinderApp/Helper/Commons.cs
+++ WpfMiniProject/NaverMovieFinderApp/Helper/Commons.cs
@@ -4,18 +4,21 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
 using NLog;
+using Logger = NLog.Logger;
+
 
 
 namespace NaverMovieFinderApp.Helper
 {
     public class Commons
     {
-        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public static  Logger LOGGER = LogManager.GetCurrentClassLogger();
 
         public static async Task<MessageDialogResult> showMessageAsync(
             string title, string message , MessageDialogStyle style = MessageDialogStyle.Affirmative)
@@ -48,6 +51,23 @@ namespace NaverMovieFinderApp.Helper
             }
 
             return result;
+        }
+
+        public static string StripHtmlTag(string text)
+        {
+            return Regex.Replace(text, @"<(.|\n)*?>", ""); //  html 특수문자 제거하는 정규표현식 
+        }
+
+
+        public static string StripPipe(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return "";
+            else
+                return text.Substring(0, text.LastIndexOf("|")).Replace("|", ", ");
+
+            //string result = text.Replace("|", ", ");
+            //return result.Substring(0, result.LastIndexOf(",")) ;
         }
     }
 
--- WpfMiniProject/NaverMovieFinderApp/MainWindow.xaml.cs
+++ WpfMiniProject/NaverMovieFinderApp/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using MahApps.Metro.Controls;
 using NaverMovieFinderApp.Helper;
+using NaverMovieFinderApp.Model;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +33,7 @@ namespace NaverMovieFinderApp
         private void BtnSearch_Click(object sender, RoutedEventArgs e)
         {
             Stsresult.Content = "";
+            ImgPoster.Source = new BitmapImage(new Uri("No_Picture.jpg", UriKind.RelativeOrAbsolute));
 
 
             if (string.IsNullOrEmpty(TxtMovieName.Text))
@@ -38,13 +42,193 @@ namespace NaverMovieFinderApp
                 return;
             }
 
-            Commons.showMessageAsync("결과", $"{TxtMovieName.Text}");
-            
+
+            try
+            {
+                ProcessSearchNaverApi(TxtMovieName.Text);
+            }
+            catch (Exception ex)
+            {
+                Commons.showMessageAsync("예외", $"예외발생 : {ex}");
+                // 로그 추가 commons
+            }
+
+
+            //Commons.showMessageAsync("결과", $"{TxtMovieName.Text}");
+
+        }
+
+        private void ProcessSearchNaverApi(string movieName)
+        {
+            string clientID = "xQofXLMDFh2ic38jZyYI";
+            string clientSecret = "pSneKCSHPF";
+            string openApiUrl = $"https://openapi.naver.com/v1/search/movie?start=1&display30&query={movieName}";
+
+            string resJson = Commons.GetRequestApi(openApiUrl, clientID, clientSecret);
+            var parseJson = JObject.Parse(resJson);
+
+            int total = Convert.ToInt32(parseJson["total"]);
+            int display = Convert.ToInt32(parseJson["display"]);
+
+            Stsresult.Content = $"{total} 중 {display} 호출성공";
+
+            var items = parseJson["items"];
+            var Json_array = (JArray)items;
+
+            List<MovieItem> movieItems = new List<MovieItem>();
+            foreach (var item in Json_array)
+            {
+                MovieItem movie = new MovieItem(
+                    Commons.StripHtmlTag(item["title"].ToString()),
+                    item["link"].ToString(),
+                    item["image"].ToString(),
+                    item["subtitle"].ToString(),
+                    item["pubDate"].ToString(),
+                    Commons.StripPipe(item["director"].ToString()),
+                    Commons.StripPipe(item["actor"].ToString()),
+                    item["userRating"].ToString());
+                movieItems.Add(movie);
+            }
+            this.DataContext = movieItems;
+
         }
 
         private void TxtMovieName_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter) BtnSearch_Click(sender, e);
+        }
+
+        private void GrdData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //if (GrdData.SelectedItem == null)
+            //{
+            //    Commons.showMessageAsync("오류", "영화를 선택하세요");
+            //    return;
+            //}
+
+            if (GrdData.SelectedItem is MovieItem)
+            {
+                var movie = GrdData.SelectedItem as MovieItem;
+                //Commons.showMessageAsync("결과", $"{movie.Image}");
+
+                if (string.IsNullOrEmpty(movie.Image))
+                {
+                    ImgPoster.Source = new BitmapImage(new Uri("No_Picture.jpg" , UriKind.RelativeOrAbsolute));
+                    
+                }
+                else
+                {
+                    ImgPoster.Source = new BitmapImage(new Uri(movie.Image, UriKind.RelativeOrAbsolute));
+                }
+
+            }
+        }
+
+        private void BtnAddWatchList_Click(object sender, RoutedEventArgs e)
+        {
+            if(GrdData.SelectedItems.Count == 0)
+            {
+                Commons.showMessageAsync("오류", "즐겨찾기에 추가할 영화를 선택하세요(복수선택 가능)");
+                return;
+            }
+
+            List<NaverFavoriteMovies> list = new List<NaverFavoriteMovies>();
+            foreach (MovieItem item in GrdData.SelectedItems)
+            {
+                NaverFavoriteMovies temp = new NaverFavoriteMovies()
+                {
+                    Title = item.Title,
+                    Link = item.Link,
+                    Image = item.Image,
+                    SubTitle = item.SubTitle,
+                    PubDate = item.PubDate,
+                    Director = item.Director,
+                    Actor = item.Actor,
+                    UserRating = item.UserRating,
+                    RegDate = DateTime.Now
+                    
+                };
+
+                list.Add(temp);
+
+            }
+
+
+            try
+            {
+
+                using (var ctx = new OpenApiLabEntities())
+                {
+                    ctx.Set<NaverFavoriteMovies>().AddRange(list);
+                    ctx.SaveChanges();
+                }
+
+
+                Commons.showMessageAsync("저장", $"즐겨찾기 추가");
+
+
+            }
+            catch (Exception ex)
+            {
+                Commons.showMessageAsync("예외", $"예외발생 : {ex}");
+                Commons.LOGGER.Error($"예외발생 : {ex}");
+            }
+
+        }
+
+        private void BtnViewWatchList_Click(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = null;
+            TxtMovieName.Text = "";
+
+            List<MovieItem> listData = new List<MovieItem>();
+            List<NaverFavoriteMovies> list = new List<NaverFavoriteMovies>();
+
+            try
+            {
+            using (var ctx = new OpenApiLabEntities())
+            {
+                list =  ctx.NaverFavoriteMovies.ToList();
+            }
+
+            }
+            catch (Exception ex)
+            {
+                Commons.showMessageAsync("예외", $"예외발생 : {ex}");
+            }
+
+            foreach (var item in list)
+            {
+                listData.Add(new MovieItem(
+                    item.Title,
+                    item.Link,
+                    item.Image,
+                    item.SubTitle,
+                    item.PubDate,
+                    item.Director,
+                    item.Actor,
+                    item.UserRating
+                   
+                    ));
+            }
+            this.DataContext = listData;
+
+
+        }
+
+        private void BtnDeleteWatchList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnWatchTrailer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnNaverMovieLink_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
