8489b741a395cdbcb2c4aca11f2272f8c9be7409
WhiteHair-H <https://github.com/WhiteHair-H/BasicSetting.git>
2021-04-07 15:55:33 +0900

    0407 커밋

--- WpfSMSApp/WpfSMSApp/View/Store/EditStore.xaml.cs
+++ WpfSMSApp/WpfSMSApp/View/Store/EditStore.xaml.cs
@@ -21,25 +21,53 @@ namespace WpfSMSApp.View.Store
     /// <summary>
     /// MyAccount.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class AddStore : Page
+    public partial class EditStore : Page
     {
-        public AddStore()
+        private int StoreID { get; set; }
+
+        // 수정할 창고객체
+        private Model.Store CurrentStore { get; set; }
+
+        public EditStore()
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// 추가생성자. StoreList에서 storeId를 받아옴
+        /// </summary>
+        /// <param name="storeId"></param>
+        public EditStore(int storeId) : this()
+        {
+            StoreID = storeId;
+        }
+
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-                LblStoreName.Visibility = LblStoreLocation.Visibility = Visibility.Hidden;
-                TxtStoreID.Text = LblStoreName.Text = LblStoreLocation.Text = "";
+            LblStoreName.Visibility = LblStoreLocation.Visibility = Visibility.Hidden;
+            TxtStoreID.Text = TxtStoreName.Text = TxtStoreLocation.Text = "";
+
+            try
+            {
+                // Store테이블에서 내용 읽음
+                CurrentStore = Logic.DataAccess.GetStores().Where(s => s.StoreID.Equals(StoreID)).FirstOrDefault();
+                TxtStoreID.Text = CurrentStore.StoreID.ToString();
+                TxtStoreName.Text = CurrentStore.StoreName;
+                TxtStoreLocation.Text = CurrentStore.StoreLocation;
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"EditStore.xaml.cs Page_Loaded 예외발생 : {ex}");
+                Commons.ShowMessageAsync("예외", $"예외발생 : {ex}");
+            }
         }
+
         private void BtnBack_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.GoBack();
         }
 
-
-
-        private bool IsValid = true; // 지역변수 - > 전역변수
+        private bool IsValid = true; // 지역변수 --> 전역변수
 
         public bool IsValidInput()
         {
@@ -66,44 +94,32 @@ namespace WpfSMSApp.View.Store
                 LblStoreLocation.Text = "창고위치를 입력하세요";
                 IsValid = false;
             }
-            else
-            {
-                var cnt = Logic.DataAccess.GetStores().Where(u => u.StoreLocation.Equals(TxtStoreLocation.Text)).Count();
-                if (cnt > 0)
-                {
-                    LblStoreLocation.Visibility = Visibility.Visible;
-                    LblStoreLocation.Text = "중복된 창고위치가 존재합니다";
-                    IsValid = false;
-                }
-            }
+
             return IsValid;
         }
+
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
             bool isValid = true; // 입력된 값이 모두 만족하는지 판별하는 플래그
-            LblStoreName.Visibility = LblStoreLocation.Visibility =
-                   Visibility.Hidden;
+            LblStoreName.Visibility = LblStoreLocation.Visibility = Visibility.Hidden;
 
+            isValid = IsValidInput(); // 유효성체크(필수)
 
-            var store = new Model.Store();
-            isValid = IsValidInput(); // 유효성 체크(항상필수) - 개발자 필수
             if (isValid)
             {
-                //MessageBox.Show("DB 입력 처리!");
+                //MessageBox.Show("DB 입력처리!");
+                CurrentStore.StoreName = TxtStoreName.Text;
+                CurrentStore.StoreLocation = TxtStoreLocation.Text;
 
-                store.StoreName = TxtStoreName.Text;
-                store.StoreLocation = TxtStoreLocation.Text;
-              
-                
                 try
                 {
-                    var result = Logic.DataAccess.SetStore(store);
+                    var result = Logic.DataAccess.SetStore(CurrentStore);  // Logic.DataAccess.SetUser(user);
                     if (result == 0)
                     {
-
-                        // 수정안됨
-                        Commons.LOGGER.Error("AddStore.Xaml.cs 창고정보 저장오류 발생");
-                        Commons.ShowMessageAsync("오류", "저장시 오류가 발생했습니다");
+                        // 수정 안됨
+                        Commons.LOGGER.Error("AddStore.xaml.cs 창고정보 수정오류 발생");
+                        Commons.ShowMessageAsync("오류", "수정시 오류가 발생했습니다");
+                        return;
                     }
                     else
                     {
@@ -114,7 +130,6 @@ namespace WpfSMSApp.View.Store
                 {
                     Commons.LOGGER.Error($"예외발생 : {ex}");
                 }
-
             }
         }
 
--- WpfSMSApp/WpfSMSApp/View/Store/StoreList.xaml.cs
+++ WpfSMSApp/WpfSMSApp/View/Store/StoreList.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using iTextSharp.text;
 using iTextSharp.text.pdf;
 using Microsoft.Win32;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -8,6 +11,7 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using WpfSMSApp.Model;
 
 namespace WpfSMSApp.View.Store
 {
@@ -28,28 +32,31 @@ namespace WpfSMSApp.View.Store
                 // Store 테이블 데이터 읽어와야 함
                 List<Model.Store> stores = new List<Model.Store>();
                 List<Model.StockStore> stockStores = new List<Model.StockStore>();
-                List<Model.Stock> stocks = new List<Model.Stock>();
                 stores = Logic.DataAccess.GetStores(); // 수영1... 데이터 심음
-                stocks = Logic.DataAccess.GetStocks();
 
                 // stores 데이터를 StockStores로 복사
                 foreach (Model.Store item in stores)
                 {
-                    stockStores.Add(new Model.StockStore() { 
-                    StoreID = item.StoreID,
-                    StoreName = item.StoreName,
-                    StoreLocation = item.StoreLocation,
-                    ItemStatus = item.ItemStatus,
-                    TagID = item.TagID,
-                    BarcodeID = item.BarcodeID,
-                    StockQuantity = 0
-                    });
+                    var store = new Model.StockStore()
+                    {
+                        StoreID = item.StoreID,
+                        StoreName = item.StoreName,
+                        StoreLocation = item.StoreLocation,
+                        ItemStatus = item.ItemStatus,
+                        TagID = item.TagID,
+                        BarcodeID = item.BarcodeID,
+                        StockQuantity = 0
+                    };
+                    // 
+                    store.StockQuantity = Logic.DataAccess.GetStocks().Where(t => t.StoreID.Equals(store.StoreID)).Count();
+
+                    stockStores.Add(store);
                 }
 
 
 
 
-                this.DataContext = stores;
+                this.DataContext = stockStores;
             }
             catch (Exception ex)
             {
@@ -86,16 +93,94 @@ namespace WpfSMSApp.View.Store
             {
                 Commons.LOGGER.Error($"예외발생 BtnAddStore_Click : {ex}");
                 throw ex;
+
+
+                
             }
         }
 
         private void BtnEditStore_Click(object sender, RoutedEventArgs e)
         {
+            if (GrdData.SelectedItem == null)
+            {
+                Commons.ShowMessageAsync("창고수정", "수정할 창고를 선택하세요");
+                return;
+            }
 
+
+            try
+            {
+                var storeId = (GrdData.SelectedItem as Model.Store).StoreID;
+
+                NavigationService.Navigate(new EditStore(storeId));
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 BtnEditStore_Click : {ex}");
+                throw ex;
+            }
         }
 
         private void BtnExportExcel_Click(object sender, RoutedEventArgs e)
         {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Excel File (*.xlsx)|*.xlsx"; // 엑셀확장자
+            dialog.FileName = "";
+            if (dialog.ShowDialog() == true)
+            {
+                try
+                {
+                    IWorkbook workbook = new XSSFWorkbook(); // xlsx용   // new HSSFWorkbook(); // xls(이전버전용)
+                    ISheet sheet = workbook.CreateSheet("Sheet1"); // 이름변경 가능
+                                                                   // 헤더row 
+                    IRow rowHeader = sheet.CreateRow(0);
+                    ICell cell = rowHeader.CreateCell(0);
+                    cell.SetCellValue("순번");
+                    cell = rowHeader.CreateCell(1);
+                    cell.SetCellValue("창고명");
+                    cell = rowHeader.CreateCell(2);
+                    cell.SetCellValue("창고위치");
+                    cell = rowHeader.CreateCell(3);
+                    cell.SetCellValue("재고수");
+
+
+                    for (int i = 0; i < GrdData.Items.Count; i++)
+                    {
+                        IRow row = sheet.CreateRow(i + 1); // 
+                        if (GrdData.Items[i] is Model.StockStore)
+                        {
+                            var stockStore = GrdData.Items[i] as Model.StockStore;
+                            ICell dataCell = row.CreateCell(0);
+                            dataCell.SetCellValue(stockStore.StoreID);
+                            dataCell = row.CreateCell(1);
+                            dataCell.SetCellValue(stockStore.StoreName);
+                            dataCell = row.CreateCell(2);
+                            dataCell.SetCellValue(stockStore.StoreLocation);
+                            dataCell = row.CreateCell(3);
+                            dataCell.SetCellValue(stockStore.StockQuantity);
+                        }
+                    }
+
+
+
+                    // 파일저장
+                    using (var fs = new FileStream(dialog.FileName, FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        workbook.Write(fs);
+                    }
+
+                    Commons.ShowMessageAsync("엑셀저장", "엑셀export 성공!");
+
+                }
+                catch (Exception ex)
+                {
+                    Commons.ShowMessageAsync("예외", $"예외발생 : {ex}");
+                }
+
+                
+
+
+            }
 
         }
     }
